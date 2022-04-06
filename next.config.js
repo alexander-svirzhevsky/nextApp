7b5afb43858b74fc037606ed2669686233d3f0a8
Aliaksandr Svirzheyski <aliaksandr.svirzheyski@prezzee.com>
2022-04-06 13:03:39 +0300

    added paragraph component

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/
-      },
-      use: ["@svgr/webpack"]
-    });
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
     return config;
-  }
-};
+  },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, Paragraph } from "../components";
 
 export default function Home() {
   return (
@@ -7,7 +7,10 @@ export default function Home() {
       <Button arrow="right" appearance="primary">
         button
       </Button>
-      <Button appearance="ghost">button</Button>
+      <Button arrow="down" appearance="ghost">button</Button>
+      <Paragraph>Hello world</Paragraph>
+      <Paragraph size="l">L Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum debitis laboriosam sint odit impedit, explicabo distinctio maiores culpa delectus tempore dolorem, quas voluptatibus dolor mollitia veritatis possimus deleniti perspiciatis enim.</Paragraph>
+      <Paragraph size="s">S size Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit molestias mollitia nam similique quos! Doloremque, iure non minima voluptatem cumque eveniet saepe quia repellat modi vitae qui laborum enim impedit!</Paragraph>
     </div>
   );
 }
